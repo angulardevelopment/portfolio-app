fca0276f6aa295a1f544d58936009576ad0cdeb6
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-05-17 12:38:47 +0530

    links removed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,17 +10,10 @@ declare var GitHubActivity;
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  projectList = [{ name: 'menupro', imageName: 'mp1.PNG', projName: "Menupro", projDesc: "App to create restaurant menus." },
-  // {name:'salespro',imageName:'sp1.PNG',projName:"Salespro",projDesc:"App to create orders."},
-  { name: 'weather', imageName: 'w1.PNG', projName: "Weather App", projDesc: "Show weather forecast." },
-  { name: 'rtohelp', imageName: 'r2.PNG', projName: "Readytohelp", projDesc: "The simple theme to show your business and its services." },
-  { name: 'texttool', imageName: 'u1.PNG', projName: "Unit Conversion & Text tools", projDesc: "this app has various useful tools for conversions." },
+  projectList = [{ name: 'name', imageName: 'mp1.PNG', projName: "projName", projDesc: " sit amet, consectetur adipiscin" },
+  // {name:'name',imageName:'sp1.PNG',projName:"projName",projDesc:"sit amet, consectetur adipiscin."},
+  { name: 'name1', imageName: 'w1.PNG', projName: "projName", projDesc: " sit amet, consectetur adipiscin" },
 
-  { name: 'quote', imageName: 'q1.PNG', projName: "Quote Generator", projDesc: "This app will generate random quotes on click." },
-  { name: 'theme', imageName: 'st1.PNG', projName: "Simple theme", projDesc: "the simple theme to show your business." },
-
-  { name: 'hackernews', imageName: 'h1.PNG', projName: "Hackernews Clone", projDesc: "It is probably the most known developer/engineer website in the World." },
-  { name: 'js30', imageName: 'js2.PNG', projName: "JS30", projDesc: "It consists of the various project to understand programming language concepts." },
   ];
   active = true;
 
